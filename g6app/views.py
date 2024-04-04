4d960839d5f4feb574eab17d811320b6150ff1a0
Rohan Benjamin <111339030+rbv98@users.noreply.github.com>
2024-04-04 13:51:36 -0700

    Update views.py

--- g6app/views.py
+++ g6app/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 from .models import Users
-def testmysql(request):
-    user = Users.objects.all()
-    context = {
-    'userid': user[1].userid,
-    'name': user[1].name,
-    }
-    return render(request, 'home.html', context)
+def hero_page(request):
+    # Add logic to fetch data for hero page
+    return render(request, 'hero_page.html')
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        # Retrieve user based on email
+        try:
+            user = Users.objects.get(email=email)
+        except Users.DoesNotExist:
+            user = None
+        
+        if user is not None:
+            if check_password(password,user.password):
+                # Authentication successful, login the user
+                login(request, user)
+                return redirect('landing_page')
+            else:
+                error_message = 'Incorrect password'
+        else:
+            error_message = 'User does not exist'
+
+        return render(request, 'login.html', {'error_message': error_message})
+    return render(request, 'login.html')
+
+
+
+@login_required
+def landing_page(request):
+    return render(request, 'landing_page.html')
