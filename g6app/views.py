1d2f4cfdb544ab26b4c5d1014ebd7dc22fd4768a
Rohan Benjamin <111339030+rbv98@users.noreply.github.com>
2024-04-10 13:40:27 -0700

    Update views.py

--- g6app/views.py
+++ g6app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.db import connection
 from .models import Users
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import UserRegistrationForm
+
 def hero_page(request):
     # Add logic to fetch data for hero page
     return render(request, 'hero_page.html')
@@ -48,3 +51,24 @@ def user_login(request):
 @login_required
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+
+
+
+def admin_page(request):
+    results = None
+    error_message = None
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        try:
+            # Execute the raw SQL query
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                # Fetch all the rows from the cursor
+                columns = [col[0] for col in cursor.description]
+                results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        except Exception as e:
+            error_message = str(e)
+            print("Error executing query:", error_message)  
+            return render(request, 'admin_page.html', {'error_message': error_message})
+    return render(request, 'admin_page.html', {'results': results})
